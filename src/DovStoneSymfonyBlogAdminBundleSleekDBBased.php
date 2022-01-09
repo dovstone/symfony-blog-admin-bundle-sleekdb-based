c0e790577c52878c71c616d9175895aebc41b28f
stOne <silveredovoui@gmail.com>
2022-01-09 14:19:25 +0000

    Create DovStoneSymfonyBlogAdminBundleSleekDBBased.php

---
+++ src/DovStoneSymfonyBlogAdminBundleSleekDBBased.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: stOne
+ * Date: 15/03/2021
+ * Time: 12:11
+ */
+
+namespace DovStone\Bundle\SymfonyBlogAdminBundleSleekDBBased;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DovStoneSymfonyBlogAdminBundleSleekDBBased extends Bundle
+{
+}
